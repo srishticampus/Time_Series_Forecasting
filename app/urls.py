aaac67b01ba4a13372012e88a530e8da7aaf58fb
AravindPremkumar <aravindpremkumar.sics@gmail.com>
2025-03-28 10:49:49 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/',views.registration,name='register'),
+    path('login/',views.user_login,name='login'),
+    path('dashboard/',views.dashboard,name='dashboard')
+]
+
+
+
